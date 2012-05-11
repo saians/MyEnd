5175437565edb8d21a4b21de97bdf257a9be09b2
Saifullah Ansari <saians@kth.se>
2012-05-11 12:34:16 +0200

    I have added a class and joined that one with the form controls

---
+++ FirstLab/MyFirstRace/WindowsFormsApplication/Talking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+    {
+    class Talking
+        {
+        public  static int BlahBlah(string thingToSay, int numberOftimes)
+        {
+            string finalString = "";
+
+            for (int i = 0; i < numberOftimes; i++)
+            {
+                finalString = finalString + thingToSay;
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+        }
+    }
---
+++ FirstLab/MyFirstRace/WindowsFormsApplication/TalkingForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+    {
+    public partial class TalkingForm : Form
+        {
+        public TalkingForm()
+            {
+            InitializeComponent();
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+            {
+            var length  =Talking.BlahBlah(textBox1.Text, (int) numericUpDown1.Value);
+
+            MessageBox.Show("This is im getting from talking for :" + length);
+            }
+
+
+
+        }
+    }
