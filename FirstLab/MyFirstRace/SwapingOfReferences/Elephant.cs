d3917d6081983e5c418ae4cc8199d5340965054b
Saifullah Ansari <saians@kth.se>
2012-05-13 18:30:33 +0200

    commiting elephant project:swapping of object references

---
+++ FirstLab/MyFirstRace/SwapingOfReferences/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwapingOfReferences
+    {
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("The ear size of " + Name + " is :" + EarSize);
+        }
+    }
+    }
---
+++ FirstLab/MyFirstRace/SwapingOfReferences/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwapingOfReferences
+    {
+    public partial class Form1 : Form
+        {
+        private Elephant lucinda;
+        private Elephant llyod;
+
+        public Form1()
+            {
+            InitializeComponent();
+             lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+             llyod = new Elephant() { EarSize = 44, Name = "Llyod" };
+
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            llyod.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+            {
+            lucinda.WhoAmI();
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+            {
+            Elephant holder;
+            holder = llyod;
+            llyod = lucinda;
+            lucinda = holder;
+            MessageBox.Show("Objects swapped..");
+
+            }
+
+        }
+    }
