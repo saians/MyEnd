21f4c6c402384b6851dcb5580342bb65702b6a38
Saifullah Ansari <saians@kth.se>
2012-05-14 09:24:39 +0200

    some insight with randomizer

---
+++ FirstLab/MyFirstRace/MenuMaker/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MenuMaker
+    {
+    public partial class Form1 : Form
+        {
+        public Form1()
+            {
+            InitializeComponent();
+
+            MenuMaker menu = new MenuMaker() {Randomizer = new Random()};
+
+            label1.Text = menu.GetMenu();
+            label2.Text = menu.GetMenu();
+            label3.Text = menu.GetMenu();
+            label4.Text = menu.GetMenu();
+            label5.Text = menu.GetMenu();
+
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        }
+    }
---
+++ FirstLab/MyFirstRace/MenuMaker/MenuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MenuMaker
+    {
+    class MenuMaker
+        {
+        public Random Randomizer;
+
+        public string[] Meats = { "kebab", "nihari", "karahi" };
+        public string[] Veg = { "spring roll", "noodles", "rice" ,"aalo k samsosay"};
+        public string[] Dessert = { "fruits", "custurd","golaganda","gol gappay" };
+
+        public string GetMenu()
+            {
+            string RandomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string RandomVeg = Veg[Randomizer.Next(Veg.Length)];
+            string RandomDessert = Dessert[Randomizer.Next(Dessert.Length)];
+
+            return RandomMeat + " with " + RandomVeg + " and " + RandomDessert;
+            }
+        }
+    }
