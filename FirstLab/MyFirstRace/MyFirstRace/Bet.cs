f792581a25b641b13885af17ccc59450fc164892
Saifullah Ansari <saians@kth.se>
2012-05-15 10:56:07 +0200

    commiting sample listbox word game app..not working i think

---
+++ FirstLab/MyFirstRace/MyFirstRace/Bet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstRace
+    {
+    class Bet
+        {
+        public int StartingPosition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            
+        }
+
+        public  void TakeStartingPosition()
+        {
+            
+        }
+        }
+    }
